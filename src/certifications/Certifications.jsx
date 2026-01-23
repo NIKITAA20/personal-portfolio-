8b20853b37ffed894bc3b7e6a55f83d1d40eadc7
Nikita Solanki <https://github.com/nikitajitendrasolanki>
2026-01-23 17:51:49 +0530

    Updated resume view & certification animation

--- src/certifications/Certifications.jsx
+++ src/certifications/Certifications.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Lottie from "lottie-react";
+import animationData from "../assets/lottie/Under-Construction 1.json";
 import "./Certifications.css";
 
 const Certifications = () => {
   return (
     <div className="lottie-fullscreen">
-      <iframe
-        src="https://lottie.host/embed/adc0304f-1d97-40e4-ad90-e947297b413d/9tlhmDyPzw.lottie"
-        title="Coming Soon"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe>
+      <Lottie
+        animationData={animationData}
+        loop
+        className="lottie-big"
+      />
     </div>
   );
 };
